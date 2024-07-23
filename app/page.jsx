b74a32aa35bcbc2fc2d73621ab5f297e1dfaa767
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-07-23 20:27:28 +0700

    fist time connect db

---
+++ app/page.jsx
@@ -0,0 +1,36 @@
+import prisma from "@/lib/prisma";
+import styles from "./page.module.css";
+import Post from "./components/Post";
+
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return (
+    <main className={styles.main}>
+      <h1>Feed</h1>
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            authorName={post.author.name}
+          />
+        );
+      })}
+    </main>
+  );
+}
